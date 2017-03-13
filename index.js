1fa22d765abb4f1c3457e7d0c5217fa3042936da
Ben Hunter <benphunter@hotmail.com>
2017-03-14 08:33:31 +1300

    update Ref!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone(process.env.TIME_ZONE);
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in ' + process.env.TIME_ZONE +' is: ' + currentTime.toString(),
     });
 };
