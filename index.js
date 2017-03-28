e3a195864c77946e2416330898d0b417e2389810
Ben Hunter <benphunter@hotmail.com>
2017-03-28 19:09:35 +1300

    added time

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone(process.env.TIME_ZONE);
     callback(null, {
         statusCode: '200',
-        body: '<h1> <style="color:#000080;">' + 'Welcome to my ' + process.env.STAGE + ' environment! <br/>' /* +
-              'The time in ' + process.env.TIME_ZONE + ' is: ' + currentTime.toString()*/ + '<h1/>',
+        body: '<h1> <style="color:#000080;">' + 'Welcome to my ' + process.env.STAGE + ' environment! <br/>'  +
+              'The time in ' + process.env.TIME_ZONE + ' is: ' + currentTime.toString() + '<h1/>',
         headers: {
                   'Content-Type': 'text/html; charset=utf-8',
         },
